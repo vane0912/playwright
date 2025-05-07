fa02e8729e2855a1cf4648e292401773730c93dd
Vanessa Gonzalez <vanessagpro5@gmail.com>
2025-05-07 16:43:30 -0600

    Describe aquí los cambios realizados

--- tests/Travel documents/warnings_validations.spec.js
+++ tests/Travel documents/warnings_validations.spec.js
@@ -15,11 +15,7 @@ test('Appointment location error', async({page}) => {
   
     const selector_products = page.getByTestId('dropdown-general.visa_type_id');
     await selector_products.selectOption('5085')
-    const continue_step1 = page.locator('id=btnContinueSidebar')
-    await expect(continue_step1).toBeEnabled()
-    await continue_step1.click()
-    await page.waitForURL('**/australia/apply-now#step=step_2')
-    
+
     const arrival_date_visible = page.locator('[name="general.arrival_date"]')
     await expect(arrival_date_visible).toBeVisible()
     await arrival_date_visible.click()
@@ -27,8 +23,7 @@ test('Appointment location error', async({page}) => {
     await page.locator('[data-dp-element="action-next"]').click()
     await page.locator('[data-dp-element="action-next"]').click()
     await page.locator('.dp--future').filter({hasText: '12'}).first().click()
-    await expect(page.locator('[name="general.email"]')).toBeVisible()
-  
+
     const continue_sidebar = page.locator('id=btnContinueSidebar')
     await expect(continue_sidebar).toBeEnabled()
     await continue_sidebar.click()
--- tests/urls.js
+++ tests/urls.js
@@ -1,4 +1,4 @@
-const deploy_url = 'https://deploy-20250505--7dab9cae.visachinaonline.com/'
+const deploy_url = 'https://deploy-20250507--7bc66b5b.visachinaonline.com/'
 const email_test = 'automations@ivisa.com'
-let Orders = ['162','161','160'] 
+let Orders = ['105','104','102'] 
 module.exports = {deploy_url,  email_test, Orders}
