66f1952ec6a2866af20f4c30092c4d99d1f28b29
Vanessa Gonzalez <vanessagpro5@gmail.com>
2025-08-15 09:37:30 -0600

    automations update

--- tests/urls.js
+++ tests/urls.js
@@ -1,7 +1,5 @@
-const deploy_url = 'https://brad-stag-3ds-86b6478pt--678b915b.visachinaonline.com/'
+const deploy_url = 'https://deploy-20250818--b669eb20.visachinaonline.com/'
 const email_test = 'automations@ivisa.com'
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders}
 
-// Token for 1 account
-//PERCY_TOKEN=web_e648aea851d56fc122b841642886d29e5cbee58d08dd380f272d4182e9ae2629
