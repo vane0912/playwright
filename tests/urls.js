a097920d286e4aa2b02cd845739454daa62730fd
Vanessa Gonzalez <vanessagpro5@gmail.com>
2025-11-14 10:43:00 -0600

    updates

--- tests/urls.js
+++ tests/urls.js
@@ -1,4 +1,4 @@
-const deploy_url = 'https://avni-ag-pagi-86b75d7ve--72136de6.visachinaonline.com/'
+const deploy_url = 'https://deploy-20251117--d64ee6d3.visachinaonline.com/'
 const email_test = 'automations@ivisa.com'
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders}
