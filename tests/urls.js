bedbc980d9ebf48df74c2dc389c560f5acb0f18d
vane0912 <vanessagpro5@gmail.com>
2026-03-02 10:18:25 -0600

    https://deploy-20260303--9ff2fbca.visachinaonline.com/

--- tests/urls.js
+++ tests/urls.js
@@ -1,5 +1,5 @@
-const deploy_url = 'https://deploy-20260303--9ff2fbca.admin.visachinaonline.com/'
-const email_test = 'automations@ivisa.com'
+const deploy_url = 'https://deploy-20260303--9ff2fbca.visachinaonline.com/'
+const email_test = 'automations1@ivisa.com'
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders}
 
