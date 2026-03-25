fde976e0b24bc0dd11c2fcfa92a45bf7a9e5cc7c
vane0912 <vanessagpro5@gmail.com>
2026-03-25 14:43:34 -0600

    https://da-checkout-rm-old-code--c725d0b7.

--- tests/urls.js
+++ tests/urls.js
@@ -1,5 +1,5 @@
-const deploy_url = 'https://deploy-20260326--8916adb7.visachinaonline.com/'
-const general_url = 'https://deploy-20260326--8916adb7.'  
+const deploy_url = 'https://da-checkout-rm-old-code--c725d0b7.visachinaonline.com/'
+const general_url = 'https://da-checkout-rm-old-code--c725d0b7.'  
 const email_test = 'automations10@ivisa.com'
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url}
