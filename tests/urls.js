22c3832469b7183c5b1ee8ad8b249d381e481e71
vane0912 <vanessagpro5@gmail.com>
2026-03-26 15:49:35 -0600

    Analia Branch

--- tests/urls.js
+++ tests/urls.js
@@ -1,5 +1,5 @@
-const deploy_url = 'https://deploy-20260326--22f9c225.visachinaonline.com/'
-const general_url = 'https://deploy-20260326--22f9c225.'  
+const deploy_url = 'https://jamie-statsig--d4538abe.visachinaonline.com/'
+const general_url = 'https://jamie-statsig--d4538abe.'  
 const email_test = 'automations@ivisa.com'
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url}
