710a3e6a2b69a434c9882379bd397257da0041a3
vane0912 <vanessagpro5@gmail.com>
2026-03-27 10:09:28 -0600

    https://deploy-20260330--2b9dd53a.

--- tests/urls.js
+++ tests/urls.js
@@ -1,5 +1,5 @@
-const deploy_url = 'https://jamie-statsig--d4538abe.visachinaonline.com/'
-const general_url = 'https://jamie-statsig--d4538abe.'  
+const deploy_url = 'https://deploy-20260330--2b9dd53a.visachinaonline.com/'
+const general_url = 'https://deploy-20260330--2b9dd53a.'  
 const email_test = 'automations@ivisa.com'
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url}
