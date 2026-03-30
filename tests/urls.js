cf5c968ecbd70caee3399e07ebd250b07aafd6c3
vane0912 <vanessagpro5@gmail.com>
2026-03-30 12:23:51 -0600

    Legacy automations

--- tests/urls.js
+++ tests/urls.js
@@ -1,6 +1,6 @@
-const deploy_url = 'https://deploy-20260330--2b9dd53a.visachinaonline.com/'
-const general_url = 'https://deploy-20260330--2b9dd53a.'  
-const email_test = 'automations@ivisa.com'
+const deploy_url = 'https://deploy-20260331--a41f4502.visachinaonline.com/'
+const general_url = 'https://deploy-20260331--a41f4502.'  
+const email_test = 'legacyAutomations@ivisa.com'
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url}
 
