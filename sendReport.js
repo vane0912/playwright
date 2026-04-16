2c9e34049c89b95cfc949202da212c5be56e3c80
vane0912 <vanessagpro5@gmail.com>
2026-04-16 16:10:56 -0600

    try again changing webhook

--- sendReport.js
+++ sendReport.js
@@ -1,7 +1,7 @@
 import fs from 'node:fs/promises'
 
 const report = await fs.readFile('./results.json')
-const response = await fetch('http://localhost:5678/webhook/receive-report', {
+const response = await fetch('https://stumpless-strainlessly-gerardo.ngrok-free.dev/webhook/receive-report', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: report
