972b462120501aa5ed428116334f6d1a6dfd3d3f
vane0912 <vanessagpro5@gmail.com>
2026-04-16 16:11:17 -0600

    change email

--- tests/urls.js
+++ tests/urls.js
@@ -1,6 +1,6 @@
 const deploy_url = "https://deploy-20260416--7a24df5b.visachinaonline.com/"
 const general_url = "https://deploy-20260416--7a24df5b."  
-const email_test = "automations1@ivisa.com"
+const email_test = "automations123@ivisa.com"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url}
 
