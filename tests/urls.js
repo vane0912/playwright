8f814ebf597f5948c379108fb4772a5df81c3583
vane0912 <vanessagpro5@gmail.com>
2026-04-16 09:21:51 -0600

    https://deploy-20260416--21fcd7c9.

--- tests/urls.js
+++ tests/urls.js
@@ -1,6 +1,6 @@
-const deploy_url = "https://deploy-20260416--70d88ffe.visachinaonline.com/"
-const general_url = "https://deploy-20260416--70d88ffe."  
-const email_test = "automations124@ivisa.com"
+const deploy_url = "https://deploy-20260416--21fcd7c9.visachinaonline.com/"
+const general_url = "https://deploy-20260416--21fcd7c9."  
+const email_test = "automations2@ivisa.com"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url}
 
