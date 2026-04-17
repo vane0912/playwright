662a5167af00c937e5a78c53bc366b2d559bf540
vane0912 <vanessagpro5@gmail.com>
2026-04-17 16:31:19 -0600

    test command

--- tests/urls.js
+++ tests/urls.js
@@ -1,6 +1,6 @@
 const deploy_url = "https://deploy-20260420--53e90552.visachinaonline.com/"
 const general_url = "https://deploy-20260420--53e90552."  
-const email_test = "automations30@ivisa.com"
+const email_test = "automations32@ivisa.com"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url}
 
