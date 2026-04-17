c9a6edf83279abae7d473689343c1136d17af41d
vane0912 <vanessagpro5@gmail.com>
2026-04-17 17:56:29 -0600

    add percy to slack message

--- sendReport.js
+++ sendReport.js
@@ -1,10 +1,19 @@
 import fs from 'node:fs/promises'
 
-const report = await fs.readFile('./results.json')
-const response = await fetch('https://n8n-service-donk.onrender.com/webhook/receive-report', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: report
-});
-const data = await response.json();
-console.log(data);
+async function sendReportToN8N(percy_url){
+    const report = await fs.readFile('./results.json')
+    const response = await fetch('https://n8n-service-donk.onrender.com/webhook/receive-report', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            report: JSON.parse(report),
+            percy_url
+        })
+    });
+    const data = await response.json();
+}
+
+const args = process.argv.slice(2)
+
+sendReportToN8N(args[0])
+
