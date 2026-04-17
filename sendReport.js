cf2bb58bd3c17dd78a8874b59cd94d68bc676d78
vane0912 <vanessagpro5@gmail.com>
2026-04-17 11:38:02 -0600

    https://deploy-20260420--53e90552.

--- sendReport.js
+++ sendReport.js
@@ -1,7 +1,7 @@
 import fs from 'node:fs/promises'
 
 const report = await fs.readFile('./results.json')
-const response = await fetch('https://stumpless-strainlessly-gerardo.ngrok-free.dev/webhook/receive-report', {
+const response = await fetch('https://n8n-service-donk.onrender.com/webhook/receive-report', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: report
