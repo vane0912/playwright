cf0726054d3babee696dcf99fa01fca5228b5cf1
vane0912 <vanessagpro5@gmail.com>
2026-04-20 15:06:08 -0600

    update percy var

--- tests/urls.js
+++ tests/urls.js
@@ -1,6 +1,6 @@
 const deploy_url = "https://deploy-20260421--73d3d685.visachinaonline.com/"
 const general_url = "https://deploy-20260421--73d3d685."  
-const email_test = "automations123@ivisa.com"
+const email_test = "automations143@ivisa.com"
 const requester = ""
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url}
